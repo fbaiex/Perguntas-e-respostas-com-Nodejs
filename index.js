811e92aebd3d2b0804387b00994bf5a9ca7a76ee
fbaiex <fabio.aiex@hotmail.com>
2023-03-29 11:37:43 -0300

    Perguntas e respostas com Nodejs.

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const app = express();
 app.set('view engie','ejs');
 
 
-app.get("/",(req,res) =>{
+app.get("/:nome/:lang",(req,res) =>{
 
-    res.render("index.ejs");
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos",preco: 3.14},
+        {nome: "Salada de batata",preco:5},
+        {nome: "Coca-Cola",preco:1.45}
+    ]
+
+    res.render("index.ejs",{
+        nome: nome,
+        lang: lang,
+        empresa: "Stack X",
+        inscritos: 0800,
+        msg: exibirMsg,
+        produtos: produtos
+    });
 });
 
 app.listen(8080,() =>{
