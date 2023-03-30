9d3563e73fc7ed7076dbbebb5d2b938ace2eb2ef
fbaiex <fabio.aiex@hotmail.com>
2023-03-30 09:26:32 -0300

    Instalando ferramentas.

--- index.js
+++ index.js
@@ -2,28 +2,12 @@ const express = require("express");
 const app = express();
 
 app.set('view engie','ejs');
+app.use(express.static('public'));
 
 
-app.get("/:nome/:lang",(req,res) =>{
+app.get("/",(req,res) =>{
 
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "Doritos",preco: 3.14},
-        {nome: "Salada de batata",preco:5},
-        {nome: "Coca-Cola",preco:1.45}
-    ]
-
-    res.render("index.ejs",{
-        nome: nome,
-        lang: lang,
-        empresa: "Stack X",
-        inscritos: 0800,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+    res.render("index.ejs");
 });
 
 app.listen(8080,() =>{
