ab7dfb93136d607c6a5f2b4f26820fc962ba3bb8
fbaiex <fabio.aiex@hotmail.com>
2023-04-03 10:01:48 -0300

    Projeto finalizado.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 
 connection
     .authenticate()
@@ -56,10 +57,29 @@ app.get("/pergunta/:id",(req,res) => {
         where: {id: id}
     }).then(pergunta => {
         if(pergunta != undefined){
-            res.render("pergunta.ejs");
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order:[ ['id','DESC']]
+            }).then(respostas => {
+                res.render("pergunta.ejs",{
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
         }else{
             res.redirect("/");
         }
+    });
+});
+
+app.post("/responder",(req,res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() =>{
+        res.redirect("/pergunta/" + perguntaId);
     });
 });
 
